7f3bb8e79dc6617b4d5c4f4182fce561ce44eca7
itsmerit37 <itsmerit37@gmail.com>
2025-11-29 11:11:29 +0530

    first you tube video

---
+++ client/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Room from "./pages/room";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <Room />
+    </div>
+  );
+}
---
+++ client/src/app/pages/room.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+const Room = () => {
+  return (
+    <div>
+    <ReactPlayer src='https://www.youtube.com/watch?v=LXb3EKWsInQ'
+    controls={true}/>
+    </div>
+  )
+}
+
+export default Room
